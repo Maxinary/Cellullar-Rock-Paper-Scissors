ea98a712f2c5cc9623c5a9eea92c4cfce77d3c21
Max Scribner <max@toastwallpaper.com>
2022-06-12 13:17:09 -0400

    added saves w 8

--- store.js
+++ store.js
@@ -75,8 +75,122 @@ let saves = {
         color(50,50,230)
       ],
       jumps: [1,2]
-    }
-};
+    },
+    "random_six": {
+        matrixSize: 128,
+        smoothing:false,
+        init_random:true,
+        fps:24,
+        matrixCount:2,
+        ifdead:false,
+        minDefeaters:2,
+        competitors: [
+          color(230,50,50),
+          color(180,180,50),
+          color(50,230,50),
+          color(50,180,180),
+          color(50,50,230),
+          color(255,255,255)
+        ],
+        jumps: [1,2]
+      },
+
+      "random_seven": {
+          matrixSize: 128,
+          smoothing:false,
+          init_random:true,
+          fps:24,
+          matrixCount:2,
+          ifdead:false,
+          minDefeaters:1,
+          competitors: [
+            color(230,50,50),
+            color(180,180,50),
+            color(50,230,50),
+            color(50,180,180),
+            color(50,50,230),
+            color(255,255,255),
+            color(50,50,50)
+          ],
+          jumps: [1,2,3]
+        },
+        "random_eight_onejump": {
+            matrixSize: 128,
+            smoothing:false,
+            init_random:true,
+            fps:24,
+            matrixCount:2,
+            ifdead:false,
+            minDefeaters:2,
+            competitors: [
+              "#d91111",
+              "#e4ff76",
+              "#5af297",
+              "#2c6ccb",
+              "#8000a2",
+              color(255,255,255),
+              color(50,50,50),
+            ],
+            jumps: [1]
+          },
+          "random_eight_twojump_1": {
+              matrixSize: 128,
+              smoothing:false,
+              init_random:true,
+              fps:24,
+              matrixCount:2,
+              ifdead:false,
+              minDefeaters:2,
+              competitors: [
+                "#d91111",
+                "#e4ff76",
+                "#5af297",
+                "#2c6ccb",
+                "#8000a2",
+                color(255,255,255),
+                color(50,50,50),
+              ],
+              jumps: [1,2]
+            },
+            "random_eight_twojump_2": {
+                matrixSize: 128,
+                smoothing:false,
+                init_random:true,
+                fps:24,
+                matrixCount:2,
+                ifdead:false,
+                minDefeaters:2,
+                competitors: [
+                  "#d91111",
+                  "#e4ff76",
+                  "#5af297",
+                  "#2c6ccb",
+                  "#8000a2",
+                  color(255,255,255),
+                  color(50,50,50),
+                ],
+                jumps: [1,3]
+              },
+            "random_eight_threejump": {
+                matrixSize: 128,
+                smoothing:false,
+                init_random:true,
+                fps:24,
+                matrixCount:2,
+                ifdead:false,
+                minDefeaters:2,
+                competitors: [
+                  "#d91111",
+                  "#e4ff76",
+                  "#5af297",
+                  "#2c6ccb",
+                  "#8000a2",
+                  color(255,255,255),
+                  color(50,50,50),
+                ],
+                jumps: [1,2,3]
+              }
+        };
 
 async function readNibbleFile(name) {
   let path = `presets/${name}.nbl`;
